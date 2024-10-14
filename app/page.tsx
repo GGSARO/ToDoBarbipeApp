84849d11561e9dc4e66c20a9da45b0897a317f1e
GGSARO <gdp_saro@outlook.com>
2024-10-14 10:48:44 -0600

    <

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { TodoAppComponent } from "@/components/todo-app";
+import InstallPrompt from './../components/InstallPrompt';
 
 export default function Home() {
   return (
     <div>
       <TodoAppComponent></TodoAppComponent>
+      <InstallPrompt></InstallPrompt>
     </div>
   );
 }
